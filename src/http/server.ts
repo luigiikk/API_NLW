7fb1e85f54b161317bd8699e4a6a8d9f46524608
luigiikk <luigimee15@gmail.com>
2024-09-10 00:46:58 -0300

    feat: add useCase for creating goals

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,37 @@
 import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { createGoal } from "../useCases/create-goal";
+import z from "zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.get("/", (req, res) => {
   res.send("Ola mundo");
 });
-
+app.post(
+  "/goals",
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async (req, res) => {
+    const { title, desiredWeeklyFrequency } = req.body;
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    });
+  }
+);
 app
   .listen({
     port: 3333,
